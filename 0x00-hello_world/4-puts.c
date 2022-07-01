ecef384ac68828bc90cecaf3f4d9ef8daac8b14c
Tmm21 <miraclemotors21@gmail.com>
2022-07-01 13:44:34 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
-
-#include <ctype.h>
-
-/
-
- * main - Entry point
- *
- *  * Return: Always 0 (sucess)
- *
- *   */
-
- int main(void)
-
+int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
