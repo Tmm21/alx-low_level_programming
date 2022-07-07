86f63c0bcb10c9ecb6efd68cbf6426863642f73d
Tmm21 <miraclemotors21@gmail.com>
2022-07-07 11:27:01 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<ctype.h>
+#include "main.h"
+/**
+ * _isdigit - a function
+ * Retun - Always 0
+ */
+int _isdigit()
+{
+char ch =6;
+if(isdigit(ch))
+{
+return 1
+}
+else
+return 0
+}
