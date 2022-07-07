174d27edee4e3c2385751dd09a3a37dc48643855
Tmm21 <miraclemotors21@gmail.com>
2022-07-07 12:32:16 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,13 +8,10 @@
 
 
 int main(void)
-
 {
-   int i;
-   
-   printf("Infinite loop incoming :(\n");
- 
-   i = 0;
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
 
   /**
    *  while (i < 10)
@@ -23,8 +20,6 @@ int main(void)
    *putchar(i);
    *}
    */
-   printf("Infinite loop avoided! \\o/\n");
-
-   return (0);
-
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
