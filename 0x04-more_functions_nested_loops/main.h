8793e44b906f958c6ea827c0b98584824e51379d
Tmm21 <miraclemotors21@gmail.com>
2022-07-07 11:39:42 -0700

    1

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,35 +1,34 @@
-fndef  MAIN_H
+#ifndef  MAIN_H                                                                                                                 
 
-#define  MAIN_H
+#define  MAIN_H                                                                                                                 
 
+                                                                                                                                
 
+int _putchar (char c);                                                                                                          
 
-int _putchar (char c);
+void print_alphabet(void);                                                                                                      
 
-void print_alphabet(void);
+void print_alphabet_x10(void);                                                                                                  
 
-void print_alphabet_x10(void);
+int _isupper(int c);                                                                                                            
+int _isdigit(int c)
 
-int _isupper(int c);
+int _isalpha(int c);                                                                                                            
 
-int _isdigit(int c);
+int print_sign(int n);                                                                                                          
 
-int print_sign(int n);
+int _abs(int);                                                                                                                  
 
-int _abs(int);
+int print_last_digit(int);                                                                                                      
 
-int print_last_digit(int);
+void jack_bauer(void);                                                                                                          
 
-void jack_bauer(void);
+void times_table(void);                                                                                                         
 
-void times_table(void);
+int add(int, int);                                                                                                              
 
-int add(int, int);
+void print_to_98(int n);                                                                                                        
 
-void print_to_98(int n);
+void print_times_table(int n);                                                                                                  
 
-void print_times_table(int n);
-
-
-
-#endif	/*  MAIN_H */
+#endif  /*  MAIN_H */
