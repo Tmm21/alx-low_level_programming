b4e65989bcbd06941ee411f80566ae9b4ce21cfb
Tmm21 <miraclemotors21@gmail.com>
2022-07-08 01:44:09 -0700

    1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -25,6 +25,8 @@ void jack_bauer(void);
 void times_table(void);                                                                                                         
 
 int add(int, int);                                                                                                              
-void print_to_98(int n);                
-int 2-largest_number(int a, int b, int c );             
+void print_to_98(int n);
+
+int 2-largest_number(int a, int b, int c );    
+
 #endif	/*  MAIN_H */
